e07dd9d604b50ba13ba1c385e1e1241d80662925
Wang Dong Yue <wang.dong.yue05@gmail.com>
2020-06-30 15:12:36 +0800

    Update sketch.js

--- Game Project/Project 1/sketch.js
+++ Game Project/Project 1/sketch.js
@@ -24,10 +24,18 @@ of code to draw then you've probably over done it.
 
 
 */
+var cloudx
+var cloudscale
+var treex
+var treey
 
 function setup()
 {
 	createCanvas(1024, 576);
+    cloudx = 300;
+    cloudscale = 1.25;
+    treex = 900;
+    treey = 324;
 }
 
 function draw()
@@ -40,13 +48,31 @@ function draw()
 
 	//1. a cloud in the sky
 	//... add your code here
-
+    noStroke();
+    fill(255);
+    ellipse(cloudx, 125, 50 * cloudscale, 50 * cloudscale);
+    ellipse(cloudx + 30 * cloudscale, 125, 30 * cloudscale, 30 * cloudscale);
+    ellipse(cloudx + 50 * cloudscale, 125, 20 * cloudscale, 20 * cloudscale);
+    
 	noStroke();
 	fill(255);
 	text("cloud", 200, 100);
 
 	//2. a mountain in the distance
-	//... add your code here
+	noStroke();
+    fill(128, 128, 128);
+    triangle(550, 435, 700, 100, 850, 435)
+    
+    // add snow caps to the mountain
+    noStroke();
+    fill(220, 220, 220);
+    beginShape();
+    vertex(700, 100);
+    vertex(650, 210);
+    vertex(700, 175);
+    vertex(750, 210);
+    vertex(700, 100);
+    endShape();
 
 	noStroke();
 	fill(255);
@@ -54,24 +80,55 @@ function draw()
 
 	//3. a tree
 	//... add your code here
-
+    stroke(0);
+    fill(180, 80, 0);
+    ellipse(treex, treey + 60, 40, 100);
+    fill(0, 150, 0);
+    ellipse(treex, treey, 120, 120);
+    
 	noStroke();
 	fill(255);
 	text("tree", 800, 346);
 
 	//4. a canyon
 	//NB. the canyon should go from ground-level to the bottom of the screen
-
 	//... add your code here
+    noStroke();
+    fill(222, 184, 135);
+    beginShape();
+    vertex(0, 430);
+    vertex(60, 430);
+    vertex(40, 470);
+    vertex(70, 490);
+    vertex(40, 560)
+    vertex(50, 800);
+    vertex(0, 800)
+    endShape();
+    
+    beginShape();
+    vertex(200, 430);
+    vertex(120, 430);
+    vertex(100, 470);
+    vertex(120, 490);
+    vertex(100, 560);
+    vertex(120, 800);
+    vertex(200, 800);
+    endShape();
 
+    noStroke();
+    fill(222, 184, 135)
 	noStroke();
 	fill(255);
 	text("canyon", 100, 480);
 
 	//5. a collectable token - eg. a jewel, fruit, coins
 	//... add your code here
-
-	noStroke();
+    stroke(0);
+    fill(255, 0 , 0);
+    ellipse(250, 420, 30, 30);
+    line(250, 410, 250, 400);
+	
+    noStroke();
 	fill(255);
 	text("collectable item", 400, 400);
 }
